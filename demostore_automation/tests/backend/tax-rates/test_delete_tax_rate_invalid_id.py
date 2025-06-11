36a504015ad506874f8ce17f8f176fbe2910f0a0
Cora Esedebe <cora_esedebe@hotmail.com>
2025-06-11 14:21:07 -0500

    June 11 - Fixed test_delete_tax_rate_invalid_id

--- demostore_automation/tests/backend/tax-rates/test_delete_tax_rate_invalid_id.py
+++ demostore_automation/tests/backend/tax-rates/test_delete_tax_rate_invalid_id.py
@@ -13,7 +13,14 @@ def test_delete_tax_rate_invalid_id():
       Test to verify the handling of deleting a tax rate with an invalid ID.
     """
     tax_obj = TaxHelper()
-    tax_id = random.randint(100,800)
+    tax_dao = TaxDAO()
+    random_tax_id = random.randint(100,800)
 
-    tax_api_info = tax_obj.delete_tax_rate(tax_id,expected_status_code=400)
+    #ensure tax_id is invalid
+    db_info = tax_dao.get_tax_by_id(random_tax_id)
+    while len(db_info) != 0:
+        random_tax_id = random.randint(100,800)
+        db_info = tax_dao.get_tax_by_id(random_tax_id)
+
+    tax_api_info = tax_obj.delete_tax_rate(random_tax_id,expected_status_code=400)
     assert tax_api_info['code'] == 'woocommerce_rest_invalid_id', f"tax rate delete invalid id return wrong code"
